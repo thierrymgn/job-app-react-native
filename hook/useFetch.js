fd2f47cdcdb6d6bd18ece0fd2af975edf1c57710
thierrymgn <maignan.thierry@outlook.fr>
2023-10-11 21:08:02 +0200

    add custom hook useFetch

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { RAPID_API_KEY, RAPID_API_HOST } from "@env"
+
+const rapidApiKey = RAPID_API_KEY
+const rapidApiHost = RAPID_API_HOST
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': rapidApiKey,
+            'X-RapidAPI-Host': rapidApiHost
+        },
+        params: {
+            ...query
+        }
+    }
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const response = await axios.request(options)
+            setData(response.data.data)
+        } catch (error) {
+            setError(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true)
+        fetchData()
+    }
+
+    return { data, isLoading, error, refetch }
+}
